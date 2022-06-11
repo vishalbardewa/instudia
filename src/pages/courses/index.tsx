1f134c91d75ea1b6d1b5bed0a8fa99727b53ab5f
vishalbardewa <bardewavishal@gmail.com>
2022-06-11 18:35:34 +0530

    Added WhatsApp CTA on All Courses page

--- src/pages/courses/index.tsx
+++ src/pages/courses/index.tsx
@@ -4,6 +4,7 @@ import CourseCard from '@/components/atom/CourseCard';
 import ImageBanner from '@/components/molecules/ImageBanner';
 import { courses } from '@/courses';
 import { ICourse } from '@/interfaces';
+import PageLayout from '@/layout/PageLayout';
 
 const rearrangeForFeatured = () => {
   const featuredCourses = courses.filter((course) => course.featured);
@@ -25,12 +26,27 @@ export default function Courses({ courseList }: any) {
   }, []);
 
   return (
-    <div className="relative bg-gray-50 px-4 pt-16 pb-20 sm:px-6 lg:px-8 lg:pt-24 lg:pb-28">
-      <div className="mx-auto mt-12 grid max-w-lg gap-5 lg:grid lg:max-w-none lg:grid-cols-3">
-        {courseItems?.map((course: ICourse, index: number) => {
-          if (course.featured)
+    <PageLayout>
+      <div className="relative bg-gray-50 px-4 pt-16 pb-20 sm:px-6 lg:px-8 lg:pt-24 lg:pb-28">
+        <div className="mx-auto mt-12 grid max-w-lg gap-5 lg:grid lg:max-w-none lg:grid-cols-3">
+          {courseItems?.map((course: ICourse, index: number) => {
+            if (course.featured)
+              return (
+                <ImageBanner
+                  key={index}
+                  title={course.title}
+                  imageUrl={course.imageUrl}
+                  category={course.category}
+                  href={course.href}
+                  description={course.description}
+                  slug={course.slug}
+                  price={course.price}
+                  duration={course.duration}
+                  durationUnit={course.durationUnit}
+                />
+              );
             return (
-              <ImageBanner
+              <CourseCard
                 key={index}
                 title={course.title}
                 imageUrl={course.imageUrl}
@@ -43,23 +59,10 @@ export default function Courses({ courseList }: any) {
                 durationUnit={course.durationUnit}
               />
             );
-          return (
-            <CourseCard
-              key={index}
-              title={course.title}
-              imageUrl={course.imageUrl}
-              category={course.category}
-              href={course.href}
-              description={course.description}
-              slug={course.slug}
-              price={course.price}
-              duration={course.duration}
-              durationUnit={course.durationUnit}
-            />
-          );
-        })}
+          })}
+        </div>
       </div>
-    </div>
+    </PageLayout>
   );
 }
 
