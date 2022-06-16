fb078652aa2cdb11e0b0391fda399ff2d2d5f7f6
vishalbardewa <bardewavishal@gmail.com>
2022-06-16 16:55:56 +0530

    reverted Image Optimisation Changes

---
+++ src/components/molecules/UnderConstruction.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+
+export default function UnderConstruction() {
+  return (
+    <div className="container p-10">
+      <h1 className="text-center text-xl font-bold">
+        Oops!! We understand your curiosity but this page is under construction
+      </h1>
+      <motion.div
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        className="relative mx-auto h-[20rem] w-[20rem] md:h-[30rem] md:w-[30rem] lg:h-[50rem] lg:w-[50rem]"
+      >
+        <Image
+          className="h-auto w-auto object-cover"
+          src="/assets/images/coming-soon.png"
+          alt="Instudia"
+          layout="fill"
+        />
+      </motion.div>
+    </div>
+  );
+}
---
+++ src/pages/workshops.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import UnderConstruction from '../components/molecules/UnderConstruction';
+
+export default function Workshops() {
+  return (
+    <>
+      {/* <div className="mx-24 grid grid-flow-col grid-cols-6">
+        <CardShowcase />
+      </div> */}
+      <UnderConstruction />
+    </>
+  );
+}
